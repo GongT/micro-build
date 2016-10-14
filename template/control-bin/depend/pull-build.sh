5090a7d17c8398c1f233b1eb1b2f68eafa2eafbc
GongT <gongteng524702837@gmail.com>
2016-10-14 17:18:35 +0800

    fix tester error

--- template/control-bin/depend/pull-build.sh
+++ template/control-bin/depend/pull-build.sh
@@ -1,9 +1,10 @@
-if is_image_exists "@{CONTAINER_NAME}" ; then
-	echo " >>> dependence image @{CONTAINER_NAME} exists"
+if sys_exists "@{CONTAINER_NAME}" ; then
+	echo "tried sys_exists @{CONTAINER_NAME}..."
+	echo " >>> dependence service @{CONTAINER_NAME} exists"
 elif [ "x@{GIT_URL}" == "x" ]; then
-	echo " >>> required dependence @{CONTAINER_NAME}, but not installed"
+	echo " >>> required dependence service @{CONTAINER_NAME}, but not installed"
 	exit 1
 else
-	echo " >>> deploying dependence @{CONTAINER_NAME}"
+	echo " >>> deploying dependence service @{CONTAINER_NAME}"
 	microbuild deploy "@{GIT_URL}" "@{SAVE_PATH}"
 fi
