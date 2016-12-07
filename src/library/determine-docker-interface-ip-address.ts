01faade20bc642017f581b20eb99614f690350ce
GongT <gongteng524702837@gmail.com>
2016-12-07 14:13:59 +0800

    update ip get method to avoid node bug

--- src/library/determine-docker-interface-ip-address.ts
+++ src/library/determine-docker-interface-ip-address.ts
@@ -1,26 +1,18 @@
-import {networkInterfaces} from "os";
+import {execSync} from "child_process";
 
+const ipv4 = /^\d+\.\d+\.\d+\.\d+$/;
 export function determineDockerInterfaceIpAddress(ifName: string = 'docker0') {
-	const nets = networkInterfaces();
-	if (!nets[ifName]) {
-		throw new Error(`can't find interface "${ifName}" on this system`);
+	const getCommand = `ifconfig ${ifName} | grep inet | awk '{ print $2 }'`;
+	console.log('try get interface %s ip address by `%s`: ', ifName, getCommand);
+	const ret = execSync(getCommand, {
+		encoding: 'utf8'
+	}).split(/\n/g).map(item => item.trim()).filter(item => !!item);
+	
+	if (ipv4.test(ret[0])) {
+		return ret;
+	} else if (ipv4.test(ret[1])) {
+		return [ret[1], ret[0]];
+	} else {
+		throw new Error(`can't get IPv4 address of interface "${ifName}".`);
 	}
-	
-	const ret = [];
-	
-	nets[ifName].forEach((config) => {
-		if (config.family === 'IPv4') {
-			ret[0] = config.address;
-		} else if (config.family === 'IPv6') {
-			ret[1] = config.address;
-		} else {
-			console.error('unknown if config: ', config);
-		}
-	});
-	
-	if (!ret[0]) {
-		throw new Error(`can't determine ipaddress on interface "${ifName}"`);
-	}
-	
-	return ret;
 }
