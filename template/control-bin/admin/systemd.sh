738e85ba97ac0263c6670a18a3aa6e3dfe7a1379
GongT <gongteng524702837@gmail.com>
2017-01-04 13:45:57 +0800

    remove sudo - no tty

--- template/control-bin/admin/systemd.sh
+++ template/control-bin/admin/systemd.sh
@@ -2,28 +2,28 @@
 
 function s_enable {
 	echo "enabling service @{SERVICE_NAME} thru systemctl"
-	sudo systemctl enable "@{SERVICE_NAME}"
+	systemctl enable "@{SERVICE_NAME}"
 }
 function s_disable {
 	echo "disabling service @{SERVICE_NAME} thru systemctl"
-	sudo systemctl disable "@{SERVICE_NAME}"
+	systemctl disable "@{SERVICE_NAME}"
 }
 function s_start {
-	sudo systemctl start "@{SERVICE_NAME}"
+	systemctl start "@{SERVICE_NAME}"
 }
 function sys_status_started {
 	systemctl is-active $1
 }
 function sys_start {
-	echo "sudo systemctl start $1"
-	sudo systemctl start $1
+	echo "systemctl start $1"
+	systemctl start $1
 }
 function sys_exists {
 	echo -e "\t/usr/lib/systemd/system/${1}.service" >&2
 	[ -e "/usr/lib/systemd/system/${1}.service" ]
 }
 function s_stop {
-	sudo systemctl stop "@{SERVICE_NAME}"
+	systemctl stop "@{SERVICE_NAME}"
 }
 function s_status {
 	systemctl status "@{SERVICE_NAME}"
@@ -31,7 +31,7 @@ function s_status {
 function s_install {
 	echo "installing service @{SERVICE_NAME} to systemd"
 	echo "    file: /usr/lib/systemd/system/@{SERVICE_NAME}.service"
-	cat "@{PWD}/systemd.service" | sudo tee "/usr/lib/systemd/system/@{SERVICE_NAME}.service" >/dev/null
+	cat "@{PWD}/systemd.service" | tee "/usr/lib/systemd/system/@{SERVICE_NAME}.service" >/dev/null
 	systemctl daemon-reload
 	s_enable
 }
@@ -41,7 +41,7 @@ function s_uninstall {
 	
 	echo "uninstalling service @{SERVICE_NAME} from systemd"
 	if [ -e "/usr/lib/systemd/system/@{SERVICE_NAME}.service" ]; then
-		sudo rm "/usr/lib/systemd/system/@{SERVICE_NAME}.service"
+		rm "/usr/lib/systemd/system/@{SERVICE_NAME}.service"
 		systemctl daemon-reload
 	fi
 }
--- template/control-bin/admin/upstart.sh
+++ template/control-bin/admin/upstart.sh
@@ -3,16 +3,16 @@
 function s_enable {
 	echo "enabling service @{SERVICE_NAME} by override file"
 	if [ -e "/etc/init/@{SERVICE_NAME}.override" ]; then
-		sudo rm "/etc/init/@{SERVICE_NAME}.override"
+		rm "/etc/init/@{SERVICE_NAME}.override"
 	fi
 }
 function s_disable {
 	echo "disabling service @{SERVICE_NAME} by override file"
-	echo manual | sudo tee "/etc/init/@{SERVICE_NAME}.override"
+	echo manual | tee "/etc/init/@{SERVICE_NAME}.override"
 }
 function s_start {
 	local OUTPUT=/tmp/starting
-	sudo service "@{SERVICE_NAME}" start >"${OUTPUT}" 2>&1
+	service "@{SERVICE_NAME}" start >"${OUTPUT}" 2>&1
 	RET=$?
 	if [ ${RET} -eq 1 ]; then
 		if cat "${OUTPUT}" | grep -q "is already running" ; then
@@ -26,15 +26,15 @@ function sys_status_started {
 	service $1 status -q
 }
 function sys_start {
-	echo "sudo service $1 start"
-	sudo service $1 start
+	echo "service $1 start"
+	service $1 start
 }
 function sys_exists {
 	echo -e "\t/etc/init/${1}.conf" >&2
 	[ -e "/etc/init/${1}.conf" ]
 }
 function s_stop {
-	sudo service "@{SERVICE_NAME}" stop
+	service "@{SERVICE_NAME}" stop
 }
 function s_status {
 	service "@{SERVICE_NAME}" status
@@ -42,7 +42,7 @@ function s_status {
 function s_install {
 	echo "installing service @{SERVICE_NAME} to /etc/init"
 	echo "    file: /etc/init/@{SERVICE_NAME}.conf"
-	cat "@{PWD}/upstart.conf" | sudo tee "/etc/init/@{SERVICE_NAME}.conf" >/dev/null
+	cat "@{PWD}/upstart.conf" | tee "/etc/init/@{SERVICE_NAME}.conf" >/dev/null
 }
 function s_uninstall {
 	s_enable 2>/dev/null
@@ -50,9 +50,9 @@ function s_uninstall {
 	
 	echo "uninstalling service @{SERVICE_NAME} from /etc/init"
 	if [ -e "/etc/init/@{SERVICE_NAME}.conf" ]; then
-		sudo rm "/etc/init/@{SERVICE_NAME}.conf"
+		rm "/etc/init/@{SERVICE_NAME}.conf"
 	fi
 	if [ -e "/etc/init/@{SERVICE_NAME}.override" ]; then
-		sudo rm "/etc/init/@{SERVICE_NAME}.override"
+		rm "/etc/init/@{SERVICE_NAME}.override"
 	fi
 }
