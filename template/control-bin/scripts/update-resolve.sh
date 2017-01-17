f590bab365c4570c09125db1118dfc8897a2175a
GongT <gongteng524702837@gmail.com>
2017-01-17 12:58:50 +0800

    add more dns to build dnsmasq

--- template/control-bin/scripts/update-resolve.sh
+++ template/control-bin/scripts/update-resolve.sh
@@ -4,3 +4,5 @@ then echo "something worng... no HOST_LOOP_IP set." >&2
 fi
 echo "npm layer enabled, rewrite /etc/resolv.conf"
 echo "nameserver ${HOST_LOOP_IP}" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 223.5.5.5" >> /etc/resolv.conf
