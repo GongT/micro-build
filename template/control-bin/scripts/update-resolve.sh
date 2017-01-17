141550514da8928a4ca084d142a6227239da5d8d
GongT <gongteng524702837@gmail.com>
2017-01-17 15:44:29 +0800

    fix call to tail/head error

--- template/control-bin/scripts/update-resolve.sh
+++ template/control-bin/scripts/update-resolve.sh
@@ -5,8 +5,8 @@ fi
 echo "npm layer enabled, rewrite /etc/resolv.conf"
 
 cp /etc/resolv.conf /tmp/xxxxxresolve.conf
-head /tmp/xxxxxresolve.conf -n 1 >/etc/resolv.conf
+head -n 1 /tmp/xxxxxresolve.conf >/etc/resolv.conf
 echo "nameserver ${HOST_LOOP_IP}" >>/etc/resolv.conf
-tail /tmp/xxxxxresolve.conf -n +2 >>/etc/resolv.conf
+tail -n +2 /tmp/xxxxxresolve.conf >>/etc/resolv.conf
 
 cat /etc/resolv.conf
