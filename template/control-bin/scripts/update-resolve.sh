cec8063410851da75e01498e4c0bd04a9d5120ab
GongT <gongteng524702837@gmail.com>
2017-01-17 14:54:37 +0800

    use native resolv

--- template/control-bin/scripts/update-resolve.sh
+++ template/control-bin/scripts/update-resolve.sh
@@ -3,6 +3,10 @@ then echo "something worng... no HOST_LOOP_IP set." >&2
 	exit 1
 fi
 echo "npm layer enabled, rewrite /etc/resolv.conf"
-echo "nameserver ${HOST_LOOP_IP}" > /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 100.100.2.136" >> /etc/resolv.conf
+
+cp /etc/resolv.conf /tmp/xxxxxresolve.conf
+head /tmp/xxxxxresolve.conf -n 1 >/etc/resolv.conf
+echo "nameserver ${HOST_LOOP_IP}" >>/etc/resolv.conf
+tail /tmp/xxxxxresolve.conf -n +2 >>/etc/resolv.conf
+
+cat /etc/resolv.conf
