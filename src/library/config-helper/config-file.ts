1f751f7f19373f020437f1dc7a4c228c3e35a6c5
GongT <gongteng524702837@gmail.com>
2017-03-01 19:28:47 +0800

    fix config

--- src/library/config-helper/config-file.ts
+++ src/library/config-helper/config-file.ts
@@ -23,29 +23,6 @@ export const IS_PACKAGE_DEBUG_MODE: boolean = ${isDebug? "true" : "false"};
 export const PACKAGE_SUPPORT_HTTPS: boolean = !IS_PACKAGE_DEBUG_MODE;
 
 // 试图探测客户端的状态
-let clientDebugging: boolean = IS_PACKAGE_DEBUG_MODE;
-let currentIsHttps: boolean = PACKAGE_SUPPORT_HTTPS;
-if(typeof window === 'object'){
-	if(window.hasOwnProperty('IS_DEBUG')){
-		clientDebugging = window['IS_DEBUG'];
-	} else if(global && global.process && global.process.env.NODE_ENV) {
-		clientDebugging = global.process.env.NODE_ENV !== 'production';
-	}
-	// currentIsHttps = /^https:/.test(location.href);
-} else {
-	currentIsHttps = false;
-	if(global.hasOwnProperty('JsonEnv')){
-		clientDebugging = global['JsonEnv'].isDebug;
-	} else {
-		clientDebugging = global.process.env.NODE_ENV !== 'production';
-	}
-}
-export function setDebugMode(debugMode = true){
-	clientDebugging = debugMode;
-}
-
-export const PACKAGE_USING_HTTPS: boolean = currentIsHttps;
-let https = currentIsHttps? 'https' : 'http';
 let supportHttps = PACKAGE_SUPPORT_HTTPS? 'https' : 'http';
 
 // 运行端口（默认端口省略）（在docker运行时永远省略）
@@ -54,8 +31,8 @@ export const CONFIG_PORT: string = IS_PACKAGE_DEBUG_MODE ? DEBUG_PORT : '';
 const portPart = CONFIG_PORT? ':' + CONFIG_PORT : '';
 
 // 服务器客户端通用的请求url
-export const CONFIG_BASE_DOMAIN: string = https + '://${baseDomain}' + portPart;
-export const CONFIG_BASE_DOMAIN_CLIENT: string = supportHttps + '://${baseDomain}' + portPart;
+export const CONFIG_BASE_DOMAIN: string = supportHttps + '://${baseDomain}' + portPart;
+export const CONFIG_BASE_DOMAIN_CLIENT: string = '//${baseDomain}' + portPart;
 export const CONFIG_BASE_DOMAIN_SERVER: string = 'http://${baseDomain}' + portPart;
 
 ${content}
