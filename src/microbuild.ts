099ec2df2337d36327e09db0c1d95dcb4fd2a25c
GongT <admin@gongt.me>
2017-04-27 02:31:49 +0800

    missing a file

--- src/microbuild.ts
+++ src/microbuild.ts
@@ -1,10 +1,29 @@
-#!/usr/bin/env node
 /// <reference path="./globals.d.ts"/>
+import {parser} from "./library/commands/command-microbuild";
+import {UsageHelper} from "./library/commands/argument-parser/help";
+import {__} from "./library/common/i18n";
 
-import {displayError} from "./library/common/display-error";
-require("source-map-support/register");
-process.on('uncaughtException', (err) => {
-	displayError(err.stack);
-	process.exit(1);
-});
-require('./bin');
+const argv = process.argv.slice(2);
+
+/*
+ if (args.namedParams['project']) {
+ updateCurrentDir(args.namedParams['project']);
+ }
+ 
+ callCommandFunction(args.nextConfig, args.next);
+ */
+
+const args = parser.parse(argv);
+if (!args.next || args.namedOptions.help) {
+	if (args.next) {
+		let itr = args.next;
+		while (itr.next && itr.next.nextConfig) {
+			itr = itr.next;
+		}
+		new UsageHelper(itr.nextConfig).print();
+	} else {
+		console.log('\n    [Micro Build] %s.\n', __('main.description'));
+		parser.usageInstance().print();
+	}
+}
+
