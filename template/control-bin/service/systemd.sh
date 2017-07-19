d7f0fbe5bb320d00730e29dc1e0afe48eee8e23c
GongT <admin@gongt.me>
2017-07-19 13:19:33 +0800

    detect masked

--- template/control-bin/service/systemd.sh
+++ template/control-bin/service/systemd.sh
@@ -9,38 +9,62 @@ function s_disable {
 	systemctl disable "@{SERVICE_NAME}"
 }
 function s_start {
+	s_masked "@{SERVICE_NAME}" && return
 	systemctl reset-failed "@{SERVICE_NAME}"
-	systemctl start "@{SERVICE_NAME}"
+	sys_action start "@{SERVICE_NAME}"
 }
 function sys_status_started {
+	s_masked $1 && return
 	systemctl is-active $1
 }
+function sys_action {
+	local ACTION=$1
+	local SERVICE=$2
+	echo "systemctl ${ACTION} $SERVICE"
+	local TMP_FILE="/tmp/detect-start-status.${RANDOM}"
+	systemctl ${ACTION} ${SERVICE}
+	RET=$?
+	if [ ${RET} -eq 0 ]; then
+		return 0
+	fi
+	if systemctl ${ACTION} ${SERVICE} 2>&1 | grep -q "service is masked" &>/dev/null ; then
+		return 0
+	fi
+	return ${RET}
+}
+function s_masked {
+	systemctl status $1 | grep -q 'masked (/dev/null'
+}
 function sys_start {
+	s_masked "$1" && return
 	systemctl reset-failed "$1"
-	echo "systemctl start $1"
-	systemctl start $1
+	sys_action start "$1"
 }
 function sys_restart {
+	s_masked "$1" && return
 	systemctl reset-failed "$1"
-	echo "systemctl start $1"
-	systemctl restart $1
+	sys_action restart "$1"
 }
 function sys_exists {
 	echo -e "\t/usr/lib/systemd/system/${1}.service"
 	[ -e "/usr/lib/systemd/system/${1}.service" ]
 }
 function s_stop {
+	s_masked "@{SERVICE_NAME}" && return
 	systemctl stop "@{SERVICE_NAME}"
 }
 function s_status {
+	s_masked "@{SERVICE_NAME}" && return
 	systemctl status "@{SERVICE_NAME}" --no-pager -l -n 0
 }
 function s_status_started {
+	s_masked "@{SERVICE_NAME}" && return
 	systemctl is-active "@{SERVICE_NAME}"
 }
 function s_restart {
+	s_masked "@{SERVICE_NAME}" && return
 	systemctl reset-failed "@{SERVICE_NAME}"
-	systemctl restart "@{SERVICE_NAME}"
+	sys_action restart "@{SERVICE_NAME}"
 }
 function s_install {
 	set -e
