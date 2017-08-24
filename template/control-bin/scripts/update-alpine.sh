78552f836de0c3bf86c7774cd561e9331fc9e451
GongT <admin@gongt.me>
2017-08-24 17:33:58 +0800

    apk add testing repo

--- template/control-bin/scripts/update-alpine.sh
+++ template/control-bin/scripts/update-alpine.sh
@@ -7,10 +7,14 @@ fi
 
 # http://dl-cdn.alpinelinux.org/alpine
 # http://mirrors.aliyun.com/alpine
+# http://dl-3.alpinelinux.org/alpine/edge/testing
 
 if [ "${IS_IN_CHINA}" = "yes" ] ; then
 	echo "http://mirrors.aliyun.com/alpine/v${VERSION}/main" > /etc/apk/repositories
 	echo "http://mirrors.aliyun.com/alpine/v${VERSION}/community" >> /etc/apk/repositories
+	echo "http://mirrors.aliyun.com/alpine/v${VERSION}/testing" >> /etc/apk/repositories
 else
+	echo "" >> /etc/apk/repositories
+	echo "http://dl-cdn.alpinelinux.org/alpine/v${VERSION}/testing" >> /etc/apk/repositories
 	echo "update-alpine: not in china"
 fi
